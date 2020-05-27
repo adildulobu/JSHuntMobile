764722c09ec0ecc9ccd212f0d1a461e9a3f36095
unknown <adildulobu@gmail.com>
2020-05-27 17:57:44 +0200

    JSHunt parte Mobile

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import './src/config/StatusBarConfig'
+
+import Main from './src/pages/main'
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+            screenOptions={{
+              headerTintColor: 'white',
+              headerStyle: { backgroundColor: '#da552f' },
+              headerTitleStyle: { alignSelf: 'center' },
+            }}
+      >
+        <Stack.Screen name="JSHunt" component={Main} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
